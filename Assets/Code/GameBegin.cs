1a1eaa2ecef4df74dc63158d8e03ec439751d706
Lanjun Zheng <lanjun@wustl.edu>
2022-09-30 18:06:48 -0500

    menu and showtips

--- Assets/Code/GameBegin.cs
+++ Assets/Code/GameBegin.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameBegin : MonoBehaviour
+public class Game : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Quit();
+        }
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
 }
