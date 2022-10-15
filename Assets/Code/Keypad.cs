153f209f43f4f24986e3c222a371ddc05b72f1a3
YUNHAO NI <n.yunhao@wustl.edu>
2022-10-14 21:42:21 -0500

    new born point

---
+++ Assets/Code/Keypad.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// using UnityStandarAssets.Characters.FirstPerson;
+
+public class Keypad : MonoBehaviour
+{
+//     public GameObject player;
+//     public GameObject keypadOB;
+
+//     public GameObject animate0B;
+//     public Animator ANI;
+
+//     public Text text0B;
+//     public string answer="12345";
+//     public bool animate;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//    public void Number (int number){
+//     text0B.text += number.ToString();
+//     button.Play();
+//    }
+//    public void Execute(){
+//     if(text0B.text == answer)
+//     {
+//         correct.Play();
+//         text0B.text = "Right";
+//     }
+//     else
+//     {
+//         wrong.Play();
+//         text0B.text = "Wrong";
+//     }
+//    }
+
+//    public void Clear()
+//     {
+        
+//         text0B.text ="";
+//         button.Play();
+            
+//     }
+//     public void Update(){
+//         if(text0B.text == "Right" && animate){
+//             ANI.setBool("animate",true);
+//             Debug.Log("it;s open");
+//         }
+//     }
+
+ }
+
